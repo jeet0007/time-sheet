35ebca5da9c83bb682c2ba53c8e0e9118fc4dc97
Jeet Singh <taranjeet0007@gmail.com>
2023-05-24 14:47:23 +0700

    removed leave tye

--- src/masterdata.ts
+++ src/masterdata.ts
@@ -4,7 +4,6 @@ export const projectOptions = [
     { value: '123', title: '[Support] Support' },
     { value: '127', title: '[Squad] iPaaS' },
     { value: '132', title: 'MAC-KTAXA' },
-    { value: '47', title: 'Leave' },
     { value: '51', title: '[KTAXA] Advisor Zone' },
     { value: '88', title: '[APPMAN] - AGM Life- Indo' },
     { value: '91', title: '[Sales] AgentMate' },
