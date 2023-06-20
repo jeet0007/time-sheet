ff7a8b901b9a5477a6b1847c66ebe01ed01d9414
Jeet Singh <taranjeet0007@gmail.com>
2023-06-20 17:58:42 +0700

    added placeholder

--- src/components/CreateTaskForm.tsx
+++ src/components/CreateTaskForm.tsx
@@ -18,18 +18,18 @@ export const CreateTaskForm = (props: CreateTaskProps) => {
         </ActionPanel>
       }
     >
-      <Form.TextField id="task" title="Task" autoFocus storeValue />
-      <Form.TextField id="manhours" title="Man hours" defaultValue="1" />
-      <Form.TextField id="module" title="Module" storeValue />
+      <Form.TextField id="task" title="Task" placeholder="Task" autoFocus storeValue />
+      <Form.TextField id="manhours" title="Man hours" placeholder="1" defaultValue="1" />
+      <Form.TextField id="module" title="Module" placeholder="Module" storeValue />
       <Form.Dropdown id="project" title="Project" storeValue>
         {projectOptions.map((item) => (
           <Form.Dropdown.Item key={item.value} value={item.value} title={item.title} />
         ))}
       </Form.Dropdown>
       <Form.Checkbox id="isEnhancement" label="Is Enhancement" defaultValue={false} />
-      <Form.TextField id="subTaskInput" title="Sub task" />
-      <Form.TextField id="remark" title="Remark" />
-      <Form.TextField id="crNo" title="Cr No" />
+      <Form.TextField id="subTaskInput" placeholder="subtask" title="Sub task" />
+      <Form.TextField id="remark" title="Remark" placeholder="Remark" />
+      <Form.TextField id="crNo" title="Cr No" placeholder="Cr No" />
       <Form.TextField id="date" title="Date" defaultValue={moment().format('DD-MM-YYYY')} />
       <Form.Checkbox id="repeat" label="Repeat" defaultValue={false} />
     </Form>
