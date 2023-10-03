6d746a728aec203d7ff3c4357e7ba2be23414070
Jeet Singh <taranjeet0007@gmail.com>
2023-10-03 23:09:24 +0700

    add match to find card number from [xxx]

--- src/utils/googleEvent.ts
+++ src/utils/googleEvent.ts
@@ -18,17 +18,31 @@ export const mergeDuplicateTasks = (tasks: Task[]) => {
 
 export const parseEvent = (item: Event) => {
   const summary = item.summary
-  const crNo: RegExpMatchArray | null = summary.match(/([a-zA-Z1-9]{3,10})-(\d+)/gm)
-  const module = crNo && crNo[0] ? crNo[0].split('-')[0].toUpperCase() : 'Meeting'
+  const crNoRegExp = /\[([a-zA-Z1-9]{3,10})\]/g
+  const crNoMatch = summary.match(crNoRegExp)
+  const crNo = crNoMatch ? crNoMatch[0].slice(1, -1) : ''
+  const module = crNo ? crNo.toUpperCase() : 'Meeting'
   const startTime = new Date(item.start.dateTime)
   const endTime = new Date(item.end.dateTime)
   const manhours = Math.round((endTime.getTime() - startTime.getTime()) / 1000 / 60 / 60)
+
+  let name = summary // Initialize name with the full summary
+  if (crNoMatch) {
+    name = summary.replace(crNoMatch[0], '').trim() // Remove spaces around the extracted name
+  }
+
   return {
     id: randomUUID(),
-    task: item.summary,
+    task: name,
     module,
     manhours,
-    crNo: crNo && crNo[0] ? crNo[0] : '',
+    crNo,
     date: moment(startTime).format('DD-MM-YYYY'),
   }
 }
+
+
+
+
+
+
