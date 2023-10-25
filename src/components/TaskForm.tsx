a8b112909ba1a1dd8fa582e4a377984c722ac647
Jeet Singh <taranjeet0007@gmail.com>
2023-10-26 02:15:55 +0700

    fix bug

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -8,12 +8,23 @@ export interface TaskFormProps {
   onCreate?: (task: Task) => void
   onEdit?: (values: Task) => void
 }
+const removeActionFromKeys = (values: Record<string, any>, action: string): void => {
+  Object.keys(values).forEach((key) => {
+    if (key.includes(`-${action}`)) {
+      const newKey = key.replace(`-${action}`, '')
+      values[newKey] = values[key]
+      delete values[key]
+    }
+  })
+}
 
 export const TaskForm = (props: TaskFormProps) => {
   const { pop } = useNavigation()
   const task: Task | undefined = props.task
+  const action = task?.id ? task.id : ''
   const handleSubmit = (values: Task) => {
     console.log(values)
+    removeActionFromKeys(values, action)
     if (props.onCreate) {
       props.onCreate(values)
     }
@@ -31,31 +42,48 @@ export const TaskForm = (props: TaskFormProps) => {
         </ActionPanel>
       }
     >
-      <Form.TextField id="task" title="Task" placeholder="Task" defaultValue={task?.task} autoFocus storeValue />
       <Form.TextField
-        id="date"
+        id={`task-${action}`}
+        title="Task"
+        placeholder="Task"
+        defaultValue={task?.task}
+        autoFocus
+        storeValue
+      />
+      <Form.TextField
+        id={`date-${action}`}
         placeholder="DD/MM/YYYY"
         title="Date"
         defaultValue={task?.date || moment().format('DD/MM/YYYY')}
       />
-      <Form.TextField id="manhours" title="Man hours" placeholder="1" defaultValue={`${task?.manhours || 1}`} />
       <Form.TextField
-        id="module"
+        id={`manhours-${action}`}
+        title="Man hours"
+        placeholder="1"
+        defaultValue={`${task?.manhours || 1}`}
+      />
+      <Form.TextField
+        id={`module-${action}`}
         title="Module"
         placeholder="Module"
         storeValue
         defaultValue={`${task?.module || ''}`}
       />
-      <Form.Dropdown id="project" title="Project" defaultValue={task?.project} storeValue>
+      <Form.Dropdown id={`project-${action}`} title="Project" defaultValue={task?.project} storeValue>
         {projectOptions.map((item) => (
           <Form.Dropdown.Item key={item.value} value={item.value} title={item.title} />
         ))}
       </Form.Dropdown>
-      <Form.Checkbox id="isEnhancement" label="Is Enhancement" defaultValue={task?.isEnhancement} />
-      <Form.TextField id="subTaskInput" placeholder="subtask" title="Sub task" defaultValue={task?.subTask || ''} />
-      <Form.TextField id="remark" title="Remark" placeholder="Remark" defaultValue={task?.remark || ''} />
-      <Form.TextField id="crNo" title="Cr No" placeholder="Cr No" defaultValue={task?.crNo || ''} />
-      <Form.Checkbox id="repeat" label="Repeat" defaultValue={task?.repeat || false} />
+      <Form.Checkbox id={`isEnhancement-${action}`} label="Is Enhancement" defaultValue={task?.isEnhancement} />
+      <Form.TextField
+        id={`subTaskInput-${action}`}
+        placeholder="subtask"
+        title="Sub task"
+        defaultValue={task?.subTask || ''}
+      />
+      <Form.TextField id={`remark-${action}`} title="Remark" placeholder="Remark" defaultValue={task?.remark || ''} />
+      <Form.TextField id={`crNo-${action}`} title="Cr No" placeholder="Cr No" defaultValue={task?.crNo || ''} />
+      <Form.Checkbox id={`repeat-${action}`} label="Repeat" defaultValue={task?.repeat || false} />
     </Form>
   )
 }
